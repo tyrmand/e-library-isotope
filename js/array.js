4a48c78b809bf7648aab3579aa674cb7a231852f
Michał Surmacz <michals.git@gmail.com>
2018-10-31 22:12:30 +0100

    Adds ebook database content

---
+++ js/array.js
@@ -0,0 +1,273 @@
+var book = [];
+book[0] = {
+	genre: "literature",
+	image: "alberto-moravia-konformista",
+	author: "Alberto Moravia",
+	title: "Konformista",
+	epub: true,
+	mobi: true,
+	pdf: false,
+};
+book[1] = {
+	genre: "economics",
+	image: "ha-joon-chang-zli-samarytanie",
+	author: "Ha-Joon Chang",
+	title: "Źli Samarytanie. Mit wolnego handlu i tajna historia kapitalizmu",
+	epub: true,
+	mobi: true,
+	pdf: true,
+};
+book[2] = {
+	genre: "esoterics",
+	image: "lucjan-znicz-cywilizacje-nieludzkie",
+	author: "Lucjan Znicz",
+	title: "Cywilizacje nieludzkie. Czy jesteśmy sami w kosmosie",
+	epub: false,
+	mobi: false,
+	pdf: true,
+};
+book[3] = {
+	genre: "history",
+	image: "alexandra-richie-warszawa-1944",
+	author: "Alexandra Richie",
+	title: "Warszawa 1944. Tragiczne powstanie",
+	epub: true,
+	mobi: true,
+	pdf: true,
+};
+book[4] = {
+	genre: "philosophy",
+	image: "artur-andrzejuk-czlowiek-i-dobro",
+	author: "Artur Andrzejuk",
+	title: "Człowiek i dobro",
+	epub: true,
+	mobi: true,
+	pdf: true,
+};
+book[5] = {
+	genre: "fantasy-sf",
+	image: "alastair-reynolds-arka-odkupienia-wyscig",
+	author: "Alastair Reynolds",
+	title: "Arka odkupienia. Tom 2. Wyścig",
+	epub: true,
+	mobi: true,
+	pdf: true,
+};
+book[6] = {
+	genre: "fantasy-sf",
+	image: "alastair-reynolds-migotliwa-wstega-poscig",
+	author: "Alastair Reynolds",
+	title: "Migotliwa wstęga. Tom 1. Pościg",
+	epub: true,
+	mobi: true,
+	pdf: true,
+};
+book[7] = {
+	genre: "psychology",
+	image: "tracy-alloway-trening-umyslu-dla-bystrzakow",
+	author: "Tracy Alloway",
+	title: "Trening umysłu dla bystrzaków",
+	epub: false,
+	mobi: false,
+	pdf: true,
+};
+book[8] = {
+	genre: "psychology",
+	image: "lew-starowicz-rozmowki-malzenskie",
+	author: "Zbigniew Lew-Starowicz",
+	title: "Rozmówki małżeńskie",
+	epub: false,
+	mobi: false,
+	pdf: true,
+};
+book[9] = {
+	genre: "literature",
+	image: "aly-cha-zanim-przekwitna-wisnie",
+	author: "Aly Cha",
+	title: "Zanim przekwitną wiśnie",
+	epub: true,
+	mobi: true,
+	pdf: true,
+};
+book[10] = {
+	genre: "fantasy-sf",
+	image: "matt-haig-ludzie",
+	author: "Matt Haig",
+	title: "Ludzie. Powieść dla Ziemian",
+	epub: true,
+	mobi: true,
+	pdf: true,
+};
+book[11] = {
+	genre: "documentary",
+	image: "lipczak-ludzie-z-placu",
+	author: "Aleksandra Lipczak",
+	title: "Ludzie z Placu Słońca",
+	epub: true,
+	mobi: true,
+	pdf: true,
+};
+book[12] = {
+	genre: "documentary",
+	image: "wajrak-wilki",
+	author: "Adam Wajrak",
+	title: "Wilki",
+	epub: true,
+	mobi: true,
+	pdf: true,
+};
+book[13] = {
+	genre: "informatics",
+	image: "jones-javascript-novice-to-ninja",
+	author: "Darren Jones",
+	title: "JavaScript. Novice to Ninja",
+	epub: true,
+	mobi: true,
+	pdf: true,
+};
+book[14] = {
+	genre: "literature",
+	image: "hustvedt-swiat",
+	author: "Siri Hustvedt",
+	title: "Świat w płomieniach",
+	epub: true,
+	mobi: true,
+	pdf: true,
+};
+book[15] = {
+	genre: "poetry",
+	image: "morsztyn-rozkosz",
+	author: "Hieronim Morsztyn",
+	title: "Światowa rozkosz",
+	epub: false,
+	mobi: false,
+	pdf: true,
+};
+book[16] = {
+	genre: "literature",
+	image: "assolant-niezwykle-przygody",
+	author: "Alfred Assolant",
+	title: "Niezwykłe choć prawdziwe przygody kapitana Korkorana",
+	epub: true,
+	mobi: true,
+	pdf: true,
+};
+book[17] = {
+	genre: "history",
+	image: "birgisson-czarny-wiking",
+	author: "Bergsveinn Birgisson",
+	title: "Czarny wiking",
+	epub: true,
+	mobi: true,
+	pdf: true,
+};
+book[18] = {
+	genre: "hobby",
+	image: "strybel-warzywa-owoce",
+	author: "Maria Derkacz-Strybel",
+	title: "Egzotyczne owoce i warzywa",
+	epub: false,
+	mobi: false,
+	pdf: true,
+};
+book[19] = {
+	genre: "hobby",
+	image: "mahoney-zbednik-inteligenta",
+	author: "Eliza Sarnacka-Mahoney",
+	title: "Zbędnik inteligenta",
+	epub: true,
+	mobi: true,
+	pdf: true,
+};
+book[20] = {
+	genre: "english",
+	image: "barret-unoficcial-english",
+	author: "Grant Barret",
+	title: "The Official Dictionary of Unofficial English",
+	epub: false,
+	mobi: false,
+	pdf: true,
+};
+book[21] = {
+	genre: "detective",
+	image: "riddle-atlantydzki-swiat",
+	author: "A. G. Riddle",
+	title: "Zagadka pochodzenia. Tom 3. Atlantydzki świat",
+	epub: true,
+	mobi: true,
+	pdf: false,
+};
+book[22] = {
+	genre: "culture",
+	image: "slaski-rybolostwo",
+	author: "Bolesław Śląski",
+	title: "Rybołówstwo i rybołówcy na Wiśle dawniej a dziś",
+	epub: false,
+	mobi: false,
+	pdf: true,
+};
+book[23] = {
+	genre: "literature",
+	image: "tolstoj-aelita",
+	author: "Aleksy Tołstoj",
+	title: "Aelita",
+	epub: true,
+	mobi: true,
+	pdf: false,
+};
+book[24] = {
+	genre: "medicine",
+	image: "gawande-komplikacje",
+	author: "Atul Gawande",
+	title: "Komplikacje. Zapiski chirurga o niedoskonałej nauce",
+	epub: true,
+	mobi: true,
+	pdf: true,
+};
+book[25] = {
+	genre: "science",
+	image: "hammond-70-minut",
+	author: "Claudia Hammond",
+	title: "70 minut na godzinę. Fenomen czasu",
+	epub: true,
+	mobi: true,
+	pdf: true,
+};
+book[26] = {
+	genre: "literature",
+	image: "mayhew-trawa",
+	author: "Anna Jean Mayhew",
+	title: "Sucha sierpniowa trawa",
+	epub: true,
+	mobi: true,
+	pdf: true,
+};
+book[27] = {
+	genre: "literature",
+	image: "proulx-drwale",
+	author: "Anne Proulx",
+	title: "Drwale",
+	epub: true,
+	mobi: true,
+	pdf: true,
+};
+book[28] = {
+	genre: "fantasy-sf",
+	image: "dukaj-lod",
+	author: "Jacek Dukaj",
+	title: "Lód",
+	epub: true,
+	mobi: true,
+	pdf: true,
+};
+book[29] = {
+	genre: "fantasy-sf",
+	image: "gardner-batman",
+	author: "Craig Shaw Gardner",
+	title: "Batman",
+	epub: true,
+	mobi: true,
+	pdf: false,
+};
+
+export default book;
