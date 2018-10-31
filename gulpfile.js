4337a0f1a6dc1eecaad8858babcfcb11a4aeda9b
Michał Surmacz <michals.git@gmail.com>
2018-10-31 21:56:27 +0100

    Prepares the background

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function() {
+
+    return gulp.src('scss/main.scss')
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            errLogToConsole: true,
+            outputStyle: 'expanded'
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('css'))
+
+});
+
+gulp.task('watch', function(){
+
+    gulp.start('sass');
+    gulp.watch('scss/**/*.scss', ['sass']);
+
+});
